f57e2d241d19a92aef172b4929fcb481234eab6a
prismathic <pjjsalam1999@gmail.com>
2020-07-13 23:59:10 +0100

    feat: added singleItem view

--- backend/jijiclone/sellers/views.py
+++ backend/jijiclone/sellers/views.py
@@ -1,12 +1,13 @@
 from sellers.models import Seller, Buyer, Item
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from .helpers.response import sendRes, sendError
 from .serializers import SellerSerializer, ItemSerializer
 
 
 @api_view(['POST'])
 
-def signUp(request):
+def SignUp(request):
     serializer = SellerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -18,7 +19,7 @@ def signUp(request):
 
 # Get all items of a particular seller and also create new items
 
-def itemsView(request, sellerId):
+def ItemsView(request, sellerId):
     try:
         seller = Seller.objects.get(pk=sellerId)
     except Seller.DoesNotExist:
@@ -33,10 +34,29 @@ def itemsView(request, sellerId):
     elif request.method == 'POST':
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
-            seller.items.add(serializer.data)
-            return sendRes(serializer.data, 201, "Item added successfully")
+            serializer.save()
+            # seller.items.add(serializer.validated_data)
+            return sendRes(serializer.validated_data, 201, "Item added successfully")
         return sendError(400, serializer.errors)
     
+@api_view(['GET','PUT','DELETE'])
+
+def SingleItemView(request, sellerId, slug):
+        try:
+            seller = Seller.objects.get(pk=sellerId)
+
+            item = seller.items.filter(slug=slug)
+            if not item:
+                return sendError(status_code=404)
+
+        except Seller.DoesNotExist:
+            return sendError(status_code=404)   
+
+        if request.method == 'GET':
+            serializer = ItemSerializer(item)
+            return sendRes(data=serializer.data)
+
+
 
 # class Items(APIView):
 
