f2c052b66c1926241cd9b98866fb35d66de78479
prismathic <pjjsalam1999@gmail.com>
2020-07-15 02:35:08 +0100

    fix: major fix for adding jwt authentication. changed function to class views

--- backend/jijiclone/sellers/urls.py
+++ backend/jijiclone/sellers/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('sellers', views.SignUp, name='seller-signup'),
-    path('sellers/<int:sellerId>/items', views.ItemsView, name='item-list'),
+    path('sellers/<int:sellerId>/items', views.ItemsView.as_view(), name='item-list'),
     path('sellers/<int:sellerId>/items/<str:slug>', views.SingleItemView, name='single-item'),
 ]
--- backend/jijiclone/sellers/views.py
+++ backend/jijiclone/sellers/views.py
@@ -1,38 +1,64 @@
 from sellers.models import Seller, Buyer, Item
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from .helpers.response import sendRes, sendError
 from .serializers import SellerSerializer, ItemSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions, exceptions
+import jwt
+
+
+class IsJWTAuthenticated(permissions.BasePermission):
+    message = {"error": "You are unauthorized to access this resource"}
+    
+    def has_permission(self, request, view):
+        token = request.META['HTTP_AUTHORIZATION'].split()[1]
+        try:
+            decoded_email = jwt.decode(token, 'MySecretKey', algorithms='HS256')
+        except jwt.InvalidSignatureError:
+            raise exceptions.PermissionDenied(detail=self.message)
+        
+
+        return view.email == decoded_email
 
 
 @api_view(['POST'])
-
+@permission_classes((AllowAny,))
 def SignUp(request):
     serializer = SellerSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return sendRes(data=serializer.data, status_code=201, custom_msg="Sign up successful!")
+        # Encode the email as jwt
+        token = jwt.encode({'email': serializer.validated_data['email']}, 'MySecretKey', algorithm='HS256')
+
+        return sendRes({"seller": serializer.validated_data, "token": token}, status_code=201, custom_msg="Sign up successful!")
     return sendError(403, serializer.errors)
         
 
-@api_view(['GET','POST'])
-
 # Get all items of a particular seller and also create new items
+class ItemsView(APIView):
 
-def ItemsView(request, sellerId):
-    try:
-        seller = Seller.objects.get(pk=sellerId)
-    except Seller.DoesNotExist:
-        return sendError(status_code=404)
+    permission_classes = (IsJWTAuthenticated,)
+    @property
+    def email(self):
+        if not hasattr(self, '_email'):
+            self._email = Seller.objects.get(pk=self.kwargs['sellerId'])
+        return self._email
 
-    if request.method == 'GET':
+    def get(self, request, sellerId):
         # items = seller.items.filter(is_sold=False)
+        try:
+            seller = Seller.objects.get(pk=sellerId)
+        except Seller.DoesNotExist:
+            return sendError(status_code=404)
 
         serializer = ItemSerializer(seller.items, many=True)
         return sendRes(data=serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, sellerId):
         serializer = ItemSerializer(data=request.data, context={'pk': sellerId})
         if serializer.is_valid():
             serializer.save()
@@ -41,7 +67,7 @@ def ItemsView(request, sellerId):
         return sendError(400, serializer.errors)
     
 @api_view(['GET','PUT','DELETE'])
-
+@permission_classes((IsAuthenticated, ))
 def SingleItemView(request, sellerId, slug):
         try:
             seller = Seller.objects.get(pk=sellerId)
@@ -70,14 +96,3 @@ def SingleItemView(request, sellerId, slug):
             item.delete()
             return sendRes(None,204,"Item removed successfully!")
 
-
-# class Items(APIView):
-
-#     def get(self, request, seller_id):
-#         seller = Seller.objects.get(pk=seller_id)
-#         items = seller.items.filter(is_sold=0)
-
-#         serializer = ItemSerializer(items)
-#         return
-
-# Create your views here.
