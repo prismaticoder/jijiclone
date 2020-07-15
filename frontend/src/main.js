ada3cff280afac4071d0fb7693cc7eb8876f142c
prismathic <pjjsalam1999@gmail.com>
2020-07-15 02:59:24 +0100

    feat: added all necessary dependencies for the frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
